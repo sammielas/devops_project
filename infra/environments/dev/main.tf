283cb2e3f929c0dd2cccadbae07f375e19a66041
sammielas <samflicks@gmail.com>
2025-05-29 10:07:17 +0100

    removed kubeconfig output

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -7,10 +7,10 @@ module "vpc" {
 }
 
 module "eks" {
-  source             = "../../modules/eks"
-  cluster_name       = var.cluster_name
-  cluster_version    = var.cluster_version
-  subnet_ids         = module.vpc.private_subnets
-  vpc_id             = module.vpc.vpc_id
-  environment        = var.environment
+  source          = "../../modules/eks"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  environment     = var.environment
 }
--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -3,8 +3,3 @@ output "eks_cluster_name" {
   value       = module.eks.cluster_id
 }
 
-output "kubeconfig" {
-  description = "Kubeconfig for accessing EKS cluster"
-  value       = module.eks.kubeconfig
-  sensitive   = true
-}
