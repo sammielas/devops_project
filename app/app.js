1389c05f6f0d4ef0f2bf00d3d4116afbdc7684fd
sammielas <samflicks@gmail.com>
2025-05-30 12:02:44 +0100

    fix:Convert imports to ES module syntax

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger/swagger.json');
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swagger/swagger.json' assert { type: "json" };
 
 dotenv.config();
 
@@ -10,16 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-const transactionRoutes = require('./routes/transactions');
-const userRoutes = require('./routes/users');
-const analyticsRoutes = require('./routes/analytics');
+import transactionRoutes from './routes/transactions.js';
+import userRoutes from './routes/users.js';
+import analyticsRoutes from './routes/analytics.js';
+import errorHandler from './middleware/errorHandler.js';
 
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-const errorHandler = require('./middleware/errorHandler');
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
