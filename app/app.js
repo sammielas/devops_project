2f141134532967c0e2d3bc964155cf007cf657a1
Samson Lasode <samflicks@gmail.com>
2025-06-12 09:28:37 +0100

    feature:expose /metrics endpoint using prom-client for Prometheus scraping

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import client from 'prom-client'; 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+
+// 🔥 Prometheus setup
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 import transactionRoutes from './routes/transactions.js';
 import userRoutes from './routes/users.js';
